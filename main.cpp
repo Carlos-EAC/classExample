ec8fda708cd2abd992baaca3d5fde189c056fa3f
Thermoflask7 <mateozaragozaburruel@gmail.com>
2024-11-29 13:38:38 -0600

    Update main.cpp
    change

--- main.cpp
+++ main.cpp
@@ -5,6 +5,10 @@ int n, num;
 double sum;
 cout << "How may numbers are you going to type ";
 cin>> n;
+  do {
+cout << "How may numbers are you going to type ";
+cin>> n;
+} while (n < 1 || n > 10);
 return 0;
 }
 
